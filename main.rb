97128b170473d67a1823bb3b45d36f26c698de2d
Maks2712 <49259635+Maks2712@users.noreply.github.com>
2022-08-19 09:35:46 +0000

    ready

--- main.rb
+++ main.rb
@@ -1,16 +1,21 @@
-def caesar_cipher (string=" ",shift=0)
+def caesar_cipher (string,shift)
     arr=string.downcase.split("")
-    arr.map!{|element| element=element.ord}
-    arr.map! 
-        do |element| 
-            if ((element+shift)>122)
-                element = 97+(element+shift-122)
-            end
-            if ((element+shift).between?(97,122))
-                element+=shift
-            end
+    arr.map!{|element| element=element.ord}  
+    arr.map! do |element| 
+           if element.between?(97,122)
+             if (element+shift)>122
+                element=96+((element+shift)-122)
+             else
+               element+shift
+             end
+           else
+             element
+           end
     end
     arr.map!{|element| element=element.chr}
-    arr
-    
+  arr.join('').capitalize
+  
 end
+
+a= caesar_cipher("Xifsf jt kpio!",-1)
+print a
