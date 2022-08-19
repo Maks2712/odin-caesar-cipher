69a63b8e9b2e3ba1c94e7786110dfd84490c40c8
Maks2712 <maks2712@yahoo.com>
2022-08-19 10:04:42 +0700

    first version

--- main.rb
+++ main.rb
@@ -1,2 +1,16 @@
-def caesar_cipher (string="",shift=0)
+def caesar_cipher (string=" ",shift=0)
+    arr=string.downcase.split("")
+    arr.map!{|element| element=element.ord}
+    arr.map! 
+        do |element| 
+            if ((element+shift)>122)
+                element = 97+(element+shift-122)
+            end
+            if ((element+shift).between?(97,122))
+                element+=shift
+            end
+    end
+    arr.map!{|element| element=element.chr}
+    arr
+    
 end
